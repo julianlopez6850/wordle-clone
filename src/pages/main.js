58d97a265f499d368027500ae80589dc5eeccbff
Ricardo Colom <ricardocolom@MacBook-Pro.local>
2022-02-03 15:01:41 -0500

    Changed app js and installed bootstrap

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function Main() {
-  return <div></div>;
+  return <div className="main-page m-4">
+    Test
+  </div>;
 }
 
 export default Main;
