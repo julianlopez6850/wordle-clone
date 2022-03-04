c33206a757557abe9dc251cfecba4b6f8753b9e6
Julian Lopez <julianlopez6850@gmail.com>
2022-03-03 23:05:05 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/navbar.js'
 import Main from './pages/main';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Main />
     </div>
   );
---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Navbar() {
+  return <div className="">
+    Test
+  </div>;
+}
+
+export default Navbar;
