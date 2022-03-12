f2d5348caaaca0d9be1084cc662c9f85bef273ff
Julian Lopez <julianlopez6850@gmail.com>
2022-03-12 02:25:00 -0500

    Added Reset Button
    - also formatted the code for readability.
    - commented most of the code

--- src/components/WordleGame.js
+++ src/components/WordleGame.js
@@ -2,16 +2,24 @@ import React, { useState, useEffect } from "react";
 import WordleTile from "./WordleTile";
 import "../styles/WordleGame.css";
 
-import PossibleWords from "./PossibleWords";
-import GuessableWords from "./GuessableWords";
+import PossibleWords from "./PossibleWords";      // list of words that can be the actual word
+import GuessableWords from "./GuessableWords";    // list of words that can be guessed
 
 // finding a random index to pick a word from the list of possible words
 let wordIndex = Math.floor(Math.random() * PossibleWords.length);
 let theWord = PossibleWords[wordIndex];
 
-let guesses = 0;
-let theGuess = "";
+let guesses = 0;  // holds the number of valid guesses made by the user
+let theGuess = "";  // holds valid guesses made by the user to check for correctness
+
+// guessList array holds the valid guesses that are made by the user
 let guessList = ["", "", "", "", "", ""];
+
+/* presenceList array holds the presence of each letter of each guess made by the user
+  "tile-correct" = the word is present and in the correct spot 
+  "tile-present" = the word is present, but in the wrong spot 
+  "tile-absent" = the word is absent from the word entirely
+*/
 let presenceList = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
@@ -21,47 +29,41 @@ let presenceList = [
   ["", "", "", "", ""],
 ];
 
-let isGameOver = false;
+let isGameOver = false; // flag to check if the game is over (the player guesses the correct word OR the player lost on guesses)
 
 function WordleGame() {
-  let isValid = false;
+  
+  const [userInput, setUserInput] = useState("");   // holds the user's input until the user presses enter
 
-  const [userInput, setUserInput] = useState("");
-
+  // check whether the guess is valid (5 letters? an actual word?).
   const checkGuessValidity = () => {
-    // check whether the guess is valid (5 letters? an actual word?).
-
-    if (isGameOver) {
+    
+    if (isGameOver) // if isGameOver is true, do not allow another guess to be made
+    {
       return;
     }
 
     if (userInput.length !== 5) // make sure the guess is 5 letters
     {
       console.log("Error. Input is " + userInput.length + " character(s). It should be 5.");
-      isValid = false;
     }
     else if (!GuessableWords.includes(userInput)) // make sure the guess is in the list of guessable words
     {      
       console.log("Error. The word " + userInput + " is not in our word list.");
-      isValid = false;
     }
-    else {
-      isValid = true;
-    }
-
-    if (isValid) // if the guess is valid, increment guesses and call the checkGuessCorrectness function
+    else // if both of the above conditions are met, the guess is valid. Do the following:
     {
-      theGuess = userInput;
-      guessList[guesses] = theGuess;
-      guesses++;
-      setUserInput("");
-      checkGuessCorrectness();
+      theGuess = userInput;             // initialize theGuess as userInput
+      guessList[guesses] = theGuess;    // save guess into guessList
+      guesses++;                        // increment guesses counter
+      setUserInput("");                 // empty userInput
+      checkGuessCorrectness();          // call the checkGuessCorrectness function
     }
   };
 
+  // check the guess against the real word.
   const checkGuessCorrectness = () => {
-    // check the guess against the real word.
-
+    
     // arrays to fill in the number of times each letter appears in theWord and theGuess later on
     let theWordLetters = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     let theGuessLetters = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
@@ -77,8 +79,9 @@ function WordleGame() {
 
     for (let i = 0; i < 5; i++) // For each letter in the guess...
     {
-      if (theGuess.charAt(i) === theWord.charAt(i)) {
-        // If that letter exists in the actual word and is in the correct position, set the letter to green
+      // If that letter exists in the actual word and it is in the correct position, set the letter to green
+      if (theGuess.charAt(i) === theWord.charAt(i))
+      {
         letters[i] = "tile-correct";
         theWordLetters[theWord.charCodeAt(i) - "a".charCodeAt(0)]--;
       }
@@ -90,10 +93,8 @@ function WordleGame() {
       {
         for (let j = 0; j < 5; j++) // If the letter meets the conditions to be yellow, set the letter to yellow.
         {
-          if (
-            theGuess.charAt(i) === theWord.charAt(j) &&
-            theWordLetters[theWord.charCodeAt(j) - "a".charCodeAt(0)] !== 0
-          ) {
+          if (theGuess.charAt(i) === theWord.charAt(j) && theWordLetters[theWord.charCodeAt(j) - "a".charCodeAt(0)] !== 0)
+          {
             letters[i] = "tile-present";
             theWordLetters[theWord.charCodeAt(j) - "a".charCodeAt(0)]--;
             break;
@@ -108,11 +109,14 @@ function WordleGame() {
 
     for (let i = 0; i < 5; i++)  // if any letter is not green, break. Else (all letters are green), congratulate the player for winning and set isGameOver flag to true.
     {
-      if (letters[i] !== "tile-correct") break;
-      else if (i === 4) {
+      if (letters[i] !== "tile-correct") 
+        break;
+      else if (i === 4) 
+      {
         isGameOver = true;
 
-        if (guesses === 1) {
+        if (guesses === 1) 
+        {
           console.log("Congratulations! The word was " + theWord + ". You got it in " + guesses + " guess!");
           return;
         }
@@ -122,14 +126,46 @@ function WordleGame() {
       }
     }
 
-    if (guesses === 6) {
-      // if this point is reached and guesses equals 6, tell the user they lost and set the isGameOver flag to true.
+    // if this point is reached and guesses equals 6, tell the user they lost and set the isGameOver flag to true.
+    if (guesses === 6) 
+    {
       console.log("You lost. The word was " + theWord);
       isGameOver = true;
       return;
     }
     console.log("");
   };
+
+  // reset game.
+  const resetGame = () => {
+    
+    // come up with a new word from the list of possible words
+    wordIndex = Math.floor(Math.random() * PossibleWords.length);
+    theWord = PossibleWords[wordIndex];
+
+    // reset all of the game variables to their original states
+    guesses = 0;
+    theGuess = "";
+    guessList = ["", "", "", "", "", ""];
+    presenceList = [
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+      ["", "", "", "", ""],
+    ];
+    guesses = 0;
+    isGameOver = false;
+    setUserInput(" ");
+
+    console.log("GAME RESET\n")
+  }
+
+  useEffect(() => {
+    if(userInput === " ")
+      setUserInput("");
+  }, [userInput]);
 
   return (
     <div className="wordle-game">
@@ -146,22 +182,32 @@ function WordleGame() {
               
               If the row you are on represents the number of guesses then it will be populated with tiles as you type,
               If not, then the row will be populated with tiles that were submitted to the guessList.
-  
+              
               The presence of a tile is determined from its entry in presenceList, such as if the letter t is absent,
-              present, or in the correct position of the correct word.              
+              present, or in the correct position of the correct word.
               
               */
-              <WordleTile letter={guesses === rowNum ? userInput.charAt(tileNum) : guessList[rowNum].charAt(tileNum)} presence={tile}
+              <WordleTile 
+                letter={guesses === rowNum ? userInput.charAt(tileNum).toUpperCase() : guessList[rowNum].charAt(tileNum).toUpperCase()} 
+                presence={tile}
               />
             ))}
           </div>
         ))}
       </div>
 
+      {/* User input field */}
       <div className="wordle-text-box">
-        <input type="text" value={userInput} onChange={(event) => { setUserInput(event.target.value); }} />
+        <input type="text" value={userInput} onChange={(event) => {setUserInput(event.target.value)}} />
       </div>
+      
+      {/* Submit button */}
       <input type="submit" value="Submit" onClick={checkGuessValidity} />
+      
+      {/* Reset button */}
+      <div>
+        <input type="submit" value="Reset" onClick={resetGame} />
+      </div>
     </div>
   );
 }
